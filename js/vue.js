d1e709e86777d755ab7a97896a9bb7d4369c314a
iaw39434895 <iaw39434895@h21.informatica.escoladeltreball.org>
2021-02-16 19:22:03 +0100

    APLICACION CRUD

--- js/vue.js
+++ js/vue.js
@@ -1,6 +1,15 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-      message: 'Hello Vue!'
+const app = Vue.createApp({
+  //el: '#app'
+  data() {
+    return { 
+      enteredGoalValue: '',
+      goals: []
+     };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredGoalValue);
     }
-  })
+  }
+});
+app.mount('#user-goals');
